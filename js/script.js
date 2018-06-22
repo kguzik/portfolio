7098ca9c09f23f46dd7df8af198f1184cec74ad7
kguzik <karolina.guzik08@gmail.com>
2018-06-22 14:20:19 +0200

    portfolio v1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    $(window).scroll(function(){
+        $('#my-photo').each(function(i){
+            if( $(window).scrollTop() > 300 ){
+                $(this).animate({'opacity':'1'}, 1500);
+            }
+        }); 
+    });
+
+    $('a[href*="#"]:not([href="#"]').on('click', function(){
+        $('html, body').animate({ scrollTop:
+        $(this.hash).offset().top - 40 }, 600);
+        $('.navbar-collapse').toggleClass('show');
+        $('.navbar-toggler').toggleClass('collapsed');
+    });
+
+    $('#home').on('click', function(e){
+      var $target = $('html,body');
+      $target.animate({scrollTop: $target.height() - $target.height()}, 500);
+      if ($('.navbar-collapse').hasClass('show')){
+        $('.navbar-collapse').toggleClass('show');
+        $('.navbar-toggler').toggleClass('collapsed');
+      }
+    });
+
+});
+
