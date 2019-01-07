aadd182b030de7d6e9271e1e965e70e9a4365b98
Karolina Guzik <karolina.guzik08@gmail.com>
2019-01-07 20:58:38 +0100

    updated portfolio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
     $(window).scroll(function(){
         $('#my-photo').each(function(i){
             if( $(window).scrollTop() > 300 ){
@@ -7,14 +5,7 @@ $(document).ready(function() {
             }
         }); 
     });
-
-    $('a[href*="#"]:not([href="#"]').on('click', function(){
-        $('html, body').animate({ scrollTop:
-        $(this.hash).offset().top - 40 }, 600);
-        $('.navbar-collapse').toggleClass('show');
-        $('.navbar-toggler').toggleClass('collapsed');
-    });
-
+    
     $('#home').on('click', function(e){
       var $target = $('html,body');
       $target.animate({scrollTop: $target.height() - $target.height()}, 500);
@@ -23,6 +14,14 @@ $(document).ready(function() {
         $('.navbar-toggler').toggleClass('collapsed');
       }
     });
+    //'a[href*="#"]:not([href="#"]'
+    $('.nav-link').on('click', function(){
+            $('html, body').animate({ scrollTop:
+            $(this.hash).offset().top - 40 }, 600);
+            $('.navbar-collapse').toggleClass('show');
+            $('.navbar-toggler').toggleClass('collapsed');
+    });
 
-});
+
+
 
